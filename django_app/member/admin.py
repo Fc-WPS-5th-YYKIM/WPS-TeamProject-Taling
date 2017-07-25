661211e2c4856c491599158c83211dce5d0a79f2
yykdev <assavictory@gmail.com>
2017-07-25 10:56:17 +0900

    s3, eb 셋팅

--- django_app/member/admin.py
+++ django_app/member/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 from .models import *
 
 # Register your models here.
+
+class MyUserAdmin(UserAdmin):
+    fieldsets = (
+            (None, {'fields': ('username', 'password')}),
+            (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'profile_photo')}),
+            (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                           'groups', 'user_permissions')}),
+            (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        )
 
 admin.site.register(MyUser)
 admin.site.register(Tutor)
