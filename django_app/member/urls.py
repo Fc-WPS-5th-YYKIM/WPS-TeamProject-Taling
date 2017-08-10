2e8950cc162c581c8345c9bedb009a6a2f967f4b
bbungsang <821heejin@naver.com>
2017-08-10 13:17:50 +0900

    remove confrim_password(from member model), add serializer, view, url about mypage

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -4,14 +4,15 @@ from . import views
 
 app_name = 'member'
 urlpatterns = [
-    # Normal User
+    # MyUser
     url(r'^$', views.MyUserList.as_view(), name='list-or-create'),
     url(r'^(?P<pk>[0-9]+)/$', views.MyUserDetail.as_view(), name='detail'),
 
-    url(r'^login/$', views.TalingLogin.as_view(), name='login'),
-    url(r'^login/facebook/$', views.FaceBookLogin.as_view(), name='facebook-login'),
-    url(r'^signup/$', views.TalingSignUp.as_view(), name='signup'),
-    url(r'^profile/change/(?P<username>[\w-]+)/$', views.ChangeMyProfile.as_view(), name='change-profile'),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^login/facebook/$', views.FaceBookLoginView.as_view(), name='facebook-login'),
+    url(r'^signup/$', views.SignUpView.as_view(), name='signup'),
+    url(r'^profile/(?P<user_pk>[0-9]+)/$', views.QueryMyProfile.as_view(), name='query-my-profile'),
+    url(r'^profile/change/(?P<pk>[0-9]+)/$', views.EditOrDeleteMyProfile.as_view(), name='edit-or-delete-my-profile'),
 
     # Tutor
     url(r'^tutor/register/$', views.TutorRegister.as_view(), name='tutor-register'),
--- django_app/member/views/auth.py
+++ django_app/member/views/auth.py
@@ -1,37 +1,48 @@
 import requests
 from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+from django.shortcuts import redirect
 
 from rest_framework import generics, status
-from rest_framework.generics import UpdateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.renderers import JSONRenderer
 
 from member.pagination import PostPagination
-from member.serializers import MyUserSerializer, UserCreateSerializer, TalingLoginSerializer
+from member.serializers import MyUserInfoSerializer, MyUserSerializer, TalingLoginSerializer
 from utils.access_token_test import access_token_test, debug_token
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 MyUser = get_user_model()
 
 ##
 # view 는 '사용제에게 제공될 데이터를 보는 것을 의미한다.
-#   '데이터가 어떻게 보이는가?' 에 대한 것은 필요하지 않다.
-#   '어떤 데이터가 보일것인가?' 에 집중해야한다.
+#   '데이터가 어떻게 보이는가?'에 대한 것은 필요하지 않다.
+#   '어떤 데이터가 보일것인가?'에 집중해야한다.
 ##
 
-class TalingSignUp(APIView):
-    serializer_class = UserCreateSerializer
-    # permission_classes = (AllowAny,)
+
+class SignUpView(APIView):
+    serializer_class = MyUserSerializer
+
+    # def get_object(self, pk):
+    #     try:
+    #         return Snippet.objects.get(pk=pk)
+    #     except Snippet.DoesNotExist:
+    #         raise Http404
 
     def post(self, request, format=None):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            print(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class TalingLogin(APIView):
+class LoginView(APIView):
     serializer_class = TalingLoginSerializer
 
     def post(self, request, format=None):
@@ -41,17 +52,48 @@ class TalingLogin(APIView):
         if serializer.is_valid(raise_exception=True):
             token_key = serializer.validated_data
 
-
         return Response({'token': token_key})
 
 
-class ChangeMyProfile(UpdateAPIView):
+class QueryMyProfile(APIView):
+    """마이페이지 조회"""
+    serializer_class = MyUserSerializer
+
+    def get(self, request, user_pk):
+        user = MyUser.objects.get(pk=user_pk)
+        serializer = MyUserSerializer(user)
+        # user_data = JSONRenderer().render(serializer.data)
+
+        if not request.user.is_authenticated and request.user.pk != user_pk:
+            login_url = '/member/login/'
+            redirect_url = login_url + '?next=' + request.get_full_path()
+            return redirect(redirect_url)
+        return Response(serializer.data)
+
+
+class EditOrDeleteMyProfile(RetrieveUpdateDestroyAPIView):
+    """마이페이지 수정/삭제"""
     queryset = MyUser.objects.all()
-    serializer_class = UserCreateSerializer
-    lookup_field = 'username'
+    serializer_class = MyUserSerializer
+
+    # def get(self, request, pk):
+    #     if not request.user.is_authenticated and request.user.pk != pk:
+    #         login_url = '/member/login/'
+    #         redirect_url = login_url + '?next=' + request.get_full_path()
+    #         return redirect(redirect_url)
+    def put(self, request, pk):
+        print(request.data)
+        serializer = MyUserSerializer(request.data)
+        return HttpResponse('hello word')
+    # URL keyword argument, 기본은 pk
+    # lookup_field = 'username'
+
+    # @method_decorator(login_required(login_url='/member/login/'))
+    # def dispatch(self, request, *args, **kwargs):
+    #     return super(ChangeMyProfileView, self).dispatch(*args, **kwargs)
 
 
-class FaceBookLogin(APIView):
+class FaceBookLoginView(APIView):
     def get(self, request):
         access_token = access_token_test(request)
         print(access_token)
@@ -82,13 +124,13 @@ class FaceBookLogin(APIView):
 
 class MyUserList(generics.ListCreateAPIView):
     queryset = MyUser.objects.all()
-    serializer_class = MyUserSerializer
+    serializer_class = MyUserInfoSerializer
     pagination_class = PostPagination
 
 
 class MyUserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = MyUser.objects.all()
-    serializer_class = MyUserSerializer
+    serializer_class = MyUserInfoSerializer
 
 
 # class MyUserViewSet(viewsets.ReadOnlyModelViewSet):
