40b39177bdae0dc1e9f6a7a584006e01e8ed92ce
yykdev <assavictory@gmail.com>
2017-08-19 21:38:30 +0900

    리뷰 뷰에서 리뷰 모델 임포트 안되는 현상 수정

--- django_app/regiclass/urls.py
+++ django_app/regiclass/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     url(r'^class/list/$', views.LectureList.as_view(), name='list_class'),
     url(r'^class/detail/$', views.LectureDetail.as_view(), name='detail_class'),
     url(r'^class/likeclass/$', views.LikeLecture.as_view(), name='like_class'),
-    url(r'^review/make/$', views.Review.as_view(), name='make_review'),
-    url(r'^review/list/$', views.Review.as_view(), name='list_review'),
+    url(r'^review/make/$', views.Reviews.as_view(), name='make_review'),
+    url(r'^review/list/$', views.Reviews.as_view(), name='list_review'),
 
     # 수강신청
     url(r'^talent/guide/(?P<slug>[-\w\d]+)/$', views.TalenteGuideView.as_view(), name='talent-guide'),
