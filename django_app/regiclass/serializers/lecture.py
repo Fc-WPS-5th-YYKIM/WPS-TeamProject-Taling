a4e8d40e95cd1e7aac631ea7e01be140e7f3b04f
yykdev <assavictory@gmail.com>
2017-08-20 20:58:07 +0900

    강의 수정 기능 추가

--- django_app/regiclass/serializers/lecture.py
+++ django_app/regiclass/serializers/lecture.py
@@ -10,7 +10,7 @@ __all__ = (
     'CurriculumSerializer',
     'LectureListSerializer',
     'LectureMakeSerializer',
-
+    'LectureUpdateSerializer',
 )
 
 
@@ -195,6 +195,129 @@ class LectureMakeSerializer(serializers.ModelSerializer):
                     curriculum_photo=self.validated_data['curriculum_photo'][k],
                     curriculum_desc=self.validated_data['curriculum_desc'][k]
                 )
+
+    class Meta:
+        model = Lecture
+        fields = (
+            'title',
+            'category',
+            'class_type',
+            'min_member',
+            'max_member',
+            'cover_photo',
+            'tutor_intro',
+            'class_intro',
+            'target_intro',
+            'price',
+            'basic_class_time',
+            'total_count',
+            'youtube_url1',
+            'youtube_url2',
+            'region_comment',
+            'notice',
+
+            'location1',
+            'location2',
+            'location_option',
+            'location_detail',
+            'location_etc_type',
+            'location_etc_text',
+            'class_weekday',
+            'class_time',
+
+            'lecture_photo',
+
+            'curriculum_photo',
+            'curriculum_desc',
+        )
+        read_only_fields = (
+            'lecture',
+        )
+
+
+class LectureUpdateSerializer(serializers.ModelSerializer):
+    location1 = serializers.ListField(
+        child=serializers.CharField(),
+    )
+    location2 = serializers.ListField(
+        child=serializers.CharField(),
+    )
+    location_option = serializers.ListField(
+        child=serializers.CharField(),
+    )
+    location_detail = serializers.ListField(
+        child=serializers.CharField(),
+    )
+    location_etc_type = serializers.ListField(
+        child=serializers.CharField(),
+    )
+    location_etc_text = serializers.ListField(
+        child=serializers.CharField(),
+    )
+    class_weekday = serializers.ListField(
+        child=serializers.CharField(),
+    )
+    class_time = serializers.ListField(
+        child=serializers.CharField(),
+    )
+    lecture_photo = serializers.ListField(
+        child=serializers.ImageField(),
+    )
+    curriculum_photo = serializers.ListField(
+        child=serializers.ImageField(),
+    )
+    curriculum_desc = serializers.ListField(
+        child=serializers.CharField(),
+    )
+
+    def save(self, tutor, lecture, **kwargs):
+
+        lecture.title=self.validated_data.get('title', '')
+        lecture.category=self.validated_data.get('category', '')
+        lecture.class_type=self.validated_data.get('class_type', '')
+        lecture.min_member=self.validated_data.get('min_member', '')
+        lecture.max_member=self.validated_data.get('max_member', '')
+        lecture.cover_photo=self.validated_data.get('cover_photo', '')
+        lecture.tutor_intro=self.validated_data.get('tutor_intro', '')
+        lecture.class_intro=self.validated_data.get('class_intro', '')
+        lecture.target_intro=self.validated_data.get('target_intro', '')
+        lecture.price=self.validated_data.get('price', '')
+        lecture.basic_class_time=self.validated_data.get('basic_class_time', '')
+        lecture.total_count=self.validated_data.get('total_count', '')
+        lecture.youtube_url1=self.validated_data.get('youtube_url1', '')
+        lecture.youtube_url2=self.validated_data.get('youtube_url2', '')
+        lecture.region_comment=self.validated_data.get('region_comment', '')
+        lecture.notice=self.validated_data.get('notice', '')
+        lecture.save()
+
+        lecture.locations.all().delete()
+        for i in range(len(self.validated_data['location1'])):
+            ClassLocation.objects.get_or_create(
+                lecture=lecture,
+                location1=self.validated_data['location1'][i],
+                location2=self.validated_data['location2'][i],
+                location_option=self.validated_data['location_option'][i],
+                location_detail=self.validated_data['location_detail'][i],
+                location_etc_type=self.validated_data['location_etc_type'][i],
+                location_etc_text=self.validated_data['location_etc_text'][i],
+                class_weekday=self.validated_data['class_weekday'][i],
+                class_time=self.validated_data['class_time'][i],
+            )
+
+        lecture.lecture_photos.all().delete()
+        for j in range(len(self.validated_data['lecture_photo'])):
+            LecturePhoto.objects.get_or_create(
+                lecture=lecture,
+                lecture_photo=self.validated_data['lecture_photo'][j],
+            )
+
+        lecture.curriculum.all().delete()
+        for k in range(len(self.validated_data['curriculum_photo'])):
+            Curriculum.objects.get_or_create(
+                lecture=lecture,
+                curriculum_photo=self.validated_data['curriculum_photo'][k],
+                curriculum_desc=self.validated_data['curriculum_desc'][k]
+            )
 
     class Meta:
         model = Lecture
--- django_app/regiclass/views/lecture.py
+++ django_app/regiclass/views/lecture.py
@@ -6,13 +6,15 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from member.models import Tutor
-from regiclass.models import Lecture
+from regiclass.models import Lecture, ClassLocation
 from regiclass.serializers import LectureListSerializer, LectureMakeSerializer
+from regiclass.serializers.lecture import LectureUpdateSerializer
 
 MyUser = get_user_model()
 
 __all__ = (
     'LectureMake',
+    'LcetureUpdate',
     'LectureList',
     'LectureDetail',
     'LikeLecture',
@@ -35,6 +37,29 @@ class LectureMake(APIView):
         return Response({'result': status.HTTP_400_BAD_REQUEST}, status=status.HTTP_400_BAD_REQUEST)
 
 
+class LcetureUpdate(APIView):
+    serializer_class = LectureUpdateSerializer
+
+    def post(self, request):
+        lecture_id = request.data['lecture_id']
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            try:
+                lecture = get_object_or_404(Lecture, pk=lecture_id)
+                user = MyUser.objects.get(pk=request.user.id)
+                tutor = get_object_or_404(Tutor, author=user)
+            except Lecture.DoesNotExist:
+                return Response({'result': '해당하는 강의 정보가 없습니다.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            except MyUser.DoesNotExist:
+                return Response({'result': '해당하는 유저 정보가 없습니다.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            except Tutor.DoesNotExist:
+                return Response({'result': '튜터가 아닌 사용자는 강의를 개설할 수 없습니다'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            serializer.save(tutor=tutor, lecture=lecture)
+            return_serializer = LectureListSerializer(lecture, context={'user': request.user})
+            return Response(return_serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'result': status.HTTP_400_BAD_REQUEST}, status=status.HTTP_400_BAD_REQUEST)
+
+
 class LectureList(APIView):
     serializer_class = LectureListSerializer
 
@@ -48,7 +73,7 @@ class LectureList(APIView):
             | (Q(category__contains=category))
             & (Q(state=state))
         ).order_by(order_by)
-        serializer = self.serializer_class(lecture_list, context={'user':request.user}, many=True)
+        serializer = self.serializer_class(lecture_list, context={'user': request.user}, many=True)
         return Response(serializer.data)
 
 
@@ -61,18 +86,7 @@ class LectureDetail(APIView):
             lecture = get_object_or_404(Lecture, pk=lecture_id)
         except Lecture.DoesNotExist:
             return Response({'result': '해당하는 강의 정보가 없습니다.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        serializer = self.serializer_class(lecture, context={'user':request.user})
-        return Response(serializer.data)
-
-    def patch(self, request):
-        lecture_id = request.data['lecture_id']
-        try:
-            lecture = get_object_or_404(Lecture, pk=lecture_id)
-        except Lecture.DoesNotExist:
-            return Response({'result': '해당하는 강의 정보가 없습니다.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        serializer = self.serializer_class(lecture, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+        serializer = self.serializer_class(lecture, context={'user': request.user})
         return Response(serializer.data)
 
 
